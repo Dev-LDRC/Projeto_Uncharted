dbee988dc5871cff853d3ec1063626125fecfad5
LDRandy <lucasrabelocruz@gmail.com>
2023-03-14 09:35:15 -0300

    UnchartedSite_1.0

---
+++ scripts.js
@@ -0,0 +1,66 @@
+// #######################################
+
+function bio_nathan() {
+
+     document.getElementById("nathan").style.borderColor = "white"
+     
+     document.getElementById("samuel").style.borderColor = "#21354b"
+     document.getElementById("elena").style.borderColor = "#21354b"
+     document.getElementById("victor").style.borderColor = "#21354b"
+
+     document.getElementById("bio_personagem").src = "./recursos/nathan_image.png"
+
+     document.getElementById("texto_bio").innerHTML = "<h3>Nathan Drake é um caçador de tesouros e aventureiro que viaja pelo mundo em busca de riquezas e relíquias perdidas. Nathan é retratado como um personagem charmoso e carismático, mas também como alguém que é capaz de se virar em situações difíceis.<br><br>Nathan é um ex-membro da Marinha dos Estados Unidos que se tornou um caçador de tesouros depois de abandonar o serviço militar. Ele é conhecido por sua habilidade em escalar paredes, lutar corpo a corpo e usar armas de fogo. Além disso, Nathan é um personagem muito inteligente e astuto, capaz de resolver quebra-cabeças complexos e decifrar enigmas.</h3>"
+     
+}
+
+// #######################################
+
+function bio_elena() {
+     
+     document.getElementById("elena").style.borderColor = "white"
+          
+     document.getElementById("nathan").style.borderColor = "#21354b"
+     document.getElementById("victor").style.borderColor = "#21354b"
+     document.getElementById("samuel").style.borderColor = "#21354b"
+
+     
+     document.getElementById("bio_personagem").src = "./recursos/elena_image.png"
+
+     document.getElementById("texto_bio").innerHTML = "<h3>Elena Fisher é uma jornalista ambiciosa e determinada que se torna amiga e eventualmente esposa do protagonista Nathan Drake.<br><br>Elena é retratada como uma personagem inteligente, enérgica e corajosa, com habilidades de combate e tiro precisos.Ela se envolve nas aventuras de Nathan em busca de histórias para suas reportagens e, eventualmente, acaba se envolvendo em perigosas missões de caça ao tesouro.<br><br>Ao longo da série, Elena e Nathan desenvolvem um relacionamento complicado e romântico, que é testado por suas diferenças pessoais e pela perigosidade de suas missões. Elena é uma personagem leal e determinada, disposta a arriscar tudo para ajudar seus amigos e desvendar segredos antigos.</h3>"
+     
+}
+
+// #######################################
+
+function bio_samuel() {
+     
+     document.getElementById("samuel").style.borderColor = "white"
+          
+     document.getElementById("victor").style.borderColor = "#21354b"
+     document.getElementById("nathan").style.borderColor = "#21354b"
+     document.getElementById("elena").style.borderColor = "#21354b"
+
+     document.getElementById("bio_personagem").src = "./recursos/samuel_image.png"
+
+     document.getElementById("texto_bio").innerHTML = "<h3>Samuel Drake é um personagem importante na série de Uncharted, que é o irmão mais velho do protagonista Nathan Drake. Ele é introduzido pela primeira vez no quarto jogo da série, Uncharted 4: A Thief's End.<br><br>Samuel é retratado como um personagem misterioso e enigmático, com uma personalidade impulsiva e volátil. Ele é um caçador de tesouros experiente e um velho conhecido de Nathan, que acreditava que Samuel havia morrido anos atrás durante uma missão perigosa.<br><br>Quando Samuel ressurge, ele convence Nathan a se juntar a ele em uma missão para encontrar o lendário tesouro do pirata Henry Avery, que envolve uma série de perigosas aventuras e confrontos com inimigos perigosos.<br><br>Ao longo da história, Samuel revela que tem suas próprias motivações e segredos, e sua presença tem um grande impacto no relacionamento de Nathan com sua esposa Elena Fisher. Samuel é retratado como um personagem complexo e muitas vezes ambíguo, cujas ações e intenções não são sempre claras.</h3>"
+     
+}
+
+// #######################################
+
+function bio_victor() {
+          
+     document.getElementById("victor").style.borderColor = "white"
+          
+     document.getElementById("nathan").style.borderColor = "#21354b"
+     document.getElementById("elena").style.borderColor = "#21354b"
+     document.getElementById("samuel").style.borderColor = "#21354b"
+
+     document.getElementById("bio_personagem").src = "./recursos/sulivan_image.png"
+
+     document.getElementById("texto_bio").innerHTML = "<h3>Victor Sullivan, também conhecido como Sully, é um personagem recorrente na série Uncharted. Ele é um experiente caçador de tesouros e parceiro de negócios do protagonista Nathan Drake.<br><br>Sully é retratado como um personagem astuto, inteligente e bem-humorado, com uma personalidade forte e um charme irresistível. Ele é um mentor e figura paterna para Nathan, ensinando-o sobre as complexidades do mundo dos tesouros e orientando-o em suas missões.<br><br>Sully também é conhecido por suas habilidades em pilotagem de aviões e barcos. Ele é frequentemente o responsável por conseguir informações cruciais e equipamentos para as missões de Nathan.<br><br>Ao longo da série, Sully e Nathan desenvolvem uma relação forte de amizade e confiança, mesmo em face de inimigos perigosos e reviravoltas dramáticas. Sully é um personagem muito querido pelos fãs de Uncharted, e sua presença é muitas vezes vista como um dos destaques da série.</h3>"
+     
+}
+
+// #######################################
